0c7ade6c05af559f86721a0b85f16c1d24248f65
Андрей Овчаренко <ovcharenkoav@meta.ua>
2015-11-23 21:09:22 +0200

    DJ Repository

--- src/Blank/Bundle/AdminBundle/Controller/PostController.php
+++ src/Blank/Bundle/AdminBundle/Controller/PostController.php
@@ -12,25 +12,30 @@ use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use JMS\DiExtraBundle\Annotation as DI;
 
-
 use Blank\Bundle\AdminBundle\Entity\Post;
 
 class PostController extends Controller
 {
 
    /**
-    * @DI\Inject("doctrine.orm.entity_manager")
+    * @DI\Inject("doctrine.orm.default_entity_manager")
     * @var EntityManagerInterface
     */
-   public $em;
+   private $em;
+
+
+   /**
+    * @DI\Inject("admin.repository.post")
+    * @var Blank\Bundle\AdminBundle\Entity\PostRepository
+    */
+   private $post;
 
     /**
-     * @Route("/post/{id}")
+     * @Route("/post/post/{id}")
      * @Template()
      */
     public function indexAction($id)
     {
-        //$em =  $this->container->get('doctrine.orm.default_entity_manager');
         $post = new Post();
         $post->setName('Александр');
         $post->setPhone('097 758 11 54');
@@ -44,5 +49,18 @@ class PostController extends Controller
         $this->em->flush();
         return array('post' => $post);
     }
+
+    /**
+     * @Route("/post/get/{id}")
+     * @Template()
+     */
+    public function getAction($id)
+    {
+        $post = $this->post->find($id);
+        return array('post' => $post);
+    }
+
+
+
 }
 
